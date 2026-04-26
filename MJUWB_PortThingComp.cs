98051ef0bf8b7f7fe341ef94005d547f22faea7e
User <User@DESKTOP-N23OUAL>
2026-04-26 15:22:10 +0200

    Add project files.

---
+++ MJUWB_PortThingComp.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MindJackUniqueWeaponBind
+{
+    public class MJUWB_PortThingComp : ThingComp
+    {
+        public Pawn registeredPawn = null;
+        public bool isRegistered = false;
+        //Adds the Port display to the Weapon's Basics section
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats()
+        {   
+            if (!isRegistered)
+            {   
+                //Returns the text from the XML
+                yield return new StatDrawEntry(StatCategoryDefOf.Basics, "MJUWB_PortThingComp_PortName".Translate(), "MJUWB_PortThingComp_PortStateDisconnected".Translate(), "MJUWB_PortThingComp_PortDescriptionDisconnected".Translate(), 15000);
+            }
+            //Returns the text from the XML and inserts the Pawn's name
+            else yield return new StatDrawEntry(StatCategoryDefOf.Basics, "MJUWB_PortThingComp_PortName".Translate(), "MJUWB_PortThingComp_PortStateConnectedValid".Translate(registeredPawn.NameFullColored), "MJUWB_PortThingComp_PortDescriptionConnectedValid".Translate(registeredPawn.NameFullColored), 15000);
+
+        }
+
+        //When the pawn registers the Mind Jack to the weapon's Port
+        public void ConnectToPort(Pawn pawn)
+        {
+            isRegistered = true;
+            registeredPawn = pawn;
+        }
+
+     }
+
+}
