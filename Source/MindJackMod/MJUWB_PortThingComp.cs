8a1f26814f9ecc606522ee9b20a93e8bfe1377db
toomanywyrms <jpzammit1994@gmail.com>
2026-04-28 23:39:26 +0200

    i fucking hate github

---
+++ Source/MindJackMod/MJUWB_PortThingComp.cs
@@ -0,0 +1,76 @@
+﻿using MindJackMod;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MindJackUniqueWeaponBind
+{
+    public class MJUWB_PortThingComp : ThingComp
+    {
+        public Pawn registeredPawn = null;
+        public bool isRegistered = false;
+        //Adds the Port display to the Weapon's Basics section
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats()
+        {   
+            if (!isRegistered)
+            {   
+                //Returns the text from the XML
+                yield return new StatDrawEntry(StatCategoryDefOf.Basics, "MJUWB_PortThingComp_PortName".Translate(), "MJUWB_PortThingComp_PortStateDisconnected".Translate(), "MJUWB_PortThingComp_PortDescriptionDisconnected".Translate(), 150000);
+            }
+            //Returns the text from the XML and inserts the Pawn's name
+            else yield return new StatDrawEntry(StatCategoryDefOf.Basics, "MJUWB_PortThingComp_PortName".Translate(), "MJUWB_PortThingComp_PortStateConnectedValid".Translate(registeredPawn.NameFullColored), "MJUWB_PortThingComp_PortDescriptionConnectedValid".Translate(registeredPawn.NameFullColored), 150000);
+
+        }
+
+        //When the pawn registers the Mind Jack to the weapon's Port
+        public void ConnectToPort(Pawn pawn, MJUWB_MindJack mindjack)
+        {
+            isRegistered = true;
+            registeredPawn = pawn;
+            mindjack.Severity = 3;
+
+        }
+
+        public override void Notify_Equipped(Pawn pawn)
+        {
+            if (pawn == registeredPawn)
+            {
+                CompQuality wepQuality = parent.TryGetComp<CompQuality>();
+                wepQuality.SetQuality(QualityCategory.Legendary, null);
+                pawn.health.hediffSet.TryGetHediff(MJUWB_DefOf.MJUWB_MindJackHediff, out Hediff hediff);
+                MJUWB_MindJack mindjack = (MJUWB_MindJack)hediff;
+                mindjack.Severity = 2;
+
+            }
+        }
+
+        public override void Notify_Unequipped(Pawn pawn)
+        {
+            if (pawn == registeredPawn)
+            {
+                CompQuality wepQuality = parent.TryGetComp<CompQuality>();
+                wepQuality.SetQuality(QualityCategory.Good, null);
+                pawn.health.hediffSet.TryGetHediff(MJUWB_DefOf.MJUWB_MindJackHediff, out Hediff hediff);
+                MJUWB_MindJack mindjack = (MJUWB_MindJack)hediff;
+                mindjack.Severity = 3;
+
+            }
+        }
+
+        //saving relevant info
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref isRegistered, "bool");
+            Scribe_References.Look(ref registeredPawn, "pawn");
+        }
+
+     }
+
+    
+
+}
