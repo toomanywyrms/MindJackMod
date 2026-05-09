1f59f6eff34b3985a7bd9c74b1118169f70e0ec4
toomanywyrms <jpzammit1994@gmail.com>
2026-05-09 19:05:45 +0200

    1.0 bugfix
    Fixed issue related to Raid generation and loadfolders.xml

--- Source/MindJackMod/MJUWE_RaidStrategyWorker.cs
+++ Source/MindJackMod/MJUWE_RaidStrategyWorker.cs
@@ -5,17 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.AI.Group;
+
 
 namespace MindJackMod
 {
-    //I'm like 90% sure I need this to trigger my Mindjack raid.
-    public class MJUWE_RaidStrategyWorker_ImmediateAttackMindjack : RaidStrategyWorker_ImmediateAttack
+    //This *should* force the raid to generate only when a mindkjack user is present. I'm not 
+    public class MJUWE_RaidStrategyWorker_MindjackRaid : RaidStrategyWorker_WithRequiredPawnKinds
     {
-        protected bool MatchesRequiredPawnKind(PawnKindDef kind) => kind.HasModExtension<MJUWE_DefModExtension>();
-        protected int MinRequiredPawnsForPoints(float pointsTotal, Faction faction = null)
+
+
+        //These two methids should check if at least 1 Mindjack Pawnkind is present in the raid
+        protected override bool MatchesRequiredPawnKind(PawnKindDef kind)
+        {
+            return kind.HasModExtension<MJUWE_DefModExtension>();
+        }
+        protected override int MinRequiredPawnsForPoints(float pointsTotal, Faction faction = null)
         {
             return 1;
         }
 
+        // The raid needs a lord job, so I copied the one from "RaidStrategyWorker_ImmediateAttack"
+        protected override LordJob MakeLordJob(IncidentParms parms, Map map, List<Pawn> pawns, int raidSeed)
+        {
+            IntVec3 originCell = (parms.spawnCenter.IsValid ? parms.spawnCenter : pawns[0].PositionHeld);
+            if (parms.attackTargets != null && parms.attackTargets.Count > 0)
+            {
+                return new LordJob_AssaultThings(parms.faction, parms.attackTargets);
+            }
+            if (parms.faction.HostileTo(Faction.OfPlayer))
+            {
+                return new LordJob_AssaultColony(parms.faction, canTimeoutOrFlee: parms.canTimeoutOrFlee, canKidnap: parms.canKidnap, sappers: false, useAvoidGridSmart: false, canSteal: parms.canSteal);
+            }
+            RCellFinder.TryFindRandomSpotJustOutsideColony(originCell, map, out var result);
+            return new LordJob_AssistColony(parms.faction, result);
+
+        }
+
     }
 }
+
